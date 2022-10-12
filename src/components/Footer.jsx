6b8b9b7e1b3efa794e9e1220e3159f4ebfd6aeb3
MateusSNeubarth <mateusneubarth@hotmail.com>
2022-10-12 16:19:01 -0300

    Final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="footer"><a href="http://marvel.com" target="_blank">Data provided by Marvel. &copy; 2014 Marvel</a></div>
+        <div className="footer"><a href="http://marvel.com" target="_blank" rel="noreferrer">Data provided by Marvel. &copy; 2014 Marvel</a></div>
     )
 }
